7768b1083482aee8772cd74199ef9ed0c08db4af
YehorSaz <inercoin@gmail.com>
2023-04-05 14:07:33 +0300

    homework

---
+++ homework.ts
@@ -0,0 +1,67 @@
+interface ICores {
+    flight: number;
+    core: {
+        reuse_count: number;
+        status: string;
+    }
+}
+
+interface IPayloads {
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number;
+}
+
+interface IMission {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    }
+    links: {
+        article_link: string;
+        video_link: string;
+    }
+    rocket: {
+        rocket_name: string;
+        first_stage: {
+            cores: ICores[];
+        }
+        second_stage: {
+            payloads: IPayloads[];
+        }
+    }
+}
+
+// -----------------------------------------------------------
+
+// 2) протипізувати функції:
+
+interface IUser {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const user: IUser = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
+
+function sum(a: number, b: number) {
+    return a + b
+}
+
+function showSum(a: number, b: number) {
+    console.log(a + b);
+}
+
+function incAge(someUser, inc: number | string) {
+    someUser.age += inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2, 3);
+incAge(user, 2);
